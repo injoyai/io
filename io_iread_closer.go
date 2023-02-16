578d3dde62c6e44b57a067e4faf566f690313010
钱纯净 <1113655791@qq.com>
2023-02-16 22:40:09 +0800

    101

--- io_iread_closer.go
+++ io_iread_closer.go
@@ -23,7 +23,6 @@ func NewIReadCloserWithContext(ctx context.Context, readCloser ReadCloser) *IRea
 		ClientKey:     NewClientKey(""),
 		ICloser:       NewICloserWithContext(ctx, readCloser),
 		buf:           bufio.NewReader(readCloser),
-		readChan:      make(chan []byte),
 		readFunc:      buf.ReadWithAll,
 		running:       0,
 	}
@@ -38,7 +37,6 @@ type IReadCloser struct {
 	*ClientKey
 	*ICloser
 	buf      *bufio.Reader                       //buffer
-	readChan chan []byte                         //读取数据chan
 	readFunc func(*bufio.Reader) ([]byte, error) //读取函数
 	dealFunc func(Message)                       //处理数据函数
 	running  uint32                              //是否在运行
@@ -68,37 +66,6 @@ func (this *IReadCloser) ReadByte() (byte, error) {
 // ReadAll 读取全部数据
 func (this *IReadCloser) ReadAll() ([]byte, error) {
 	return buf.ReadWithAll(this.Buffer())
-}
-
-// ReadMessage 读取数据 todo 处理不及时数据会丢失
-func (this *IReadCloser) ReadMessage() ([]byte, error) {
-	return this.ReadLast(0)
-}
-
-// ReadChan 数据通道,需要及时处理,否则会丢弃数据
-func (this *IReadCloser) ReadChan() <-chan []byte {
-	return this.readChan
-}
-
-// ReadLast 读取最新的数据
-func (this *IReadCloser) ReadLast(timeout time.Duration) (response []byte, err error) {
-	if timeout <= 0 {
-		select {
-		case <-this.Done():
-			return nil, this.Err()
-		case response = <-this.readChan:
-			return
-		}
-	}
-	t := time.NewTimer(timeout)
-	select {
-	case <-this.Done():
-		return nil, this.Err()
-	case response = <-this.readChan:
-		return
-	case <-t.C:
-		return nil, ErrWithTimeout
-	}
 }
 
 // WriteTo 写入io.Writer
@@ -181,6 +148,13 @@ func (this *IReadCloser) SetDealWithWriter(writer Writer) {
 	})
 }
 
+// SetDealWithChan 设置数据处理到chan
+func (this *IReadCloser) SetDealWithChan(c chan Message) {
+	this.SetDealFunc(func(msg Message) {
+		c <- msg
+	})
+}
+
 //================================RunTime================================
 
 // Running 是否在运行
@@ -214,11 +188,7 @@ func (this *IReadCloser) Run() error {
 				this.lastTime = time.Now()
 				//打印日志
 				this.ClientPrinter.Print(NewMessage(bytes), TagRead, this.GetKey())
-				select {
-				case this.readChan <- bytes:
-					//尝试加入队列
-				default:
-				}
+				//处理数据
 				if this.dealFunc != nil {
 					this.dealFunc(NewMessage(bytes))
 				}
