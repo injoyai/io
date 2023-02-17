9089950652a6ddb212118e12fcc3e73b01edafbd
1113655791@qq.com <q1113655791>
2023-02-17 17:03:58 +0800

    需要整理messageReader和reader的关系,以方便兼容mqtt等已经分包的数据

---
+++ io_i_read_closer2.go
@@ -0,0 +1,120 @@
+package io
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+)
+
+func NewIReaderCloser(readCloser MessageReadCloser) *IReaderCloser {
+	return NewMReadCloserWithContext(context.Background(), readCloser)
+}
+
+func NewMReadCloserWithContext(ctx context.Context, readCloser MessageReadCloser) *IReaderCloser {
+	if c, ok := readCloser.(*IReaderCloser); ok && c != nil {
+		return c
+	}
+	entity := &IReaderCloser{
+		IPrinter: NewIPrinter(),
+		IKey:     NewIKey(""),
+		ICloser:  NewICloserWithContext(ctx, readCloser),
+		running:  0,
+	}
+	entity.SetCloseFunc(func(ctx context.Context, msg Message) {
+		entity.cancel()
+	})
+	return entity
+}
+
+type IReaderCloser struct {
+	*IPrinter
+	*IKey
+	*ICloser
+	*IReader
+	dealFunc DealFunc //处理数据函数
+	running  uint32   //是否在运行
+}
+
+//================================Nature================================
+
+func (this *IReaderCloser) SetKey(key string) *IReaderCloser {
+	this.IKey.SetKey(key)
+	this.ICloser.SetKey(key)
+	return this
+}
+
+func (this *IReaderCloser) SetPrintFunc(fn PrintFunc) *IReaderCloser {
+	this.IPrinter.SetPrintFunc(fn)
+	//错误信息按ASCII编码
+	return this
+}
+
+func (this *IReaderCloser) Debug(b ...bool) *IReaderCloser {
+	this.IPrinter.Debug(b...)
+	this.ICloser.Debug(b...)
+	return this
+}
+
+//================================DealFunc================================
+
+// SetDealFunc 设置数据处理函数
+func (this *IReaderCloser) SetDealFunc(fn func(msg Message)) {
+	this.dealFunc = fn
+}
+
+// SetDealWithNil 不设置数据处理函数
+func (this *IReaderCloser) SetDealWithNil() {
+	this.SetDealFunc(nil)
+}
+
+// SetDealWithWriter 设置数据处理到io.Writer
+func (this *IReaderCloser) SetDealWithWriter(writer Writer) {
+	this.SetDealFunc(func(msg Message) {
+		writer.Write(msg)
+	})
+}
+
+// SetDealWithChan 设置数据处理到chan
+func (this *IReaderCloser) SetDealWithChan(c chan Message) {
+	this.SetDealFunc(func(msg Message) {
+		c <- msg
+	})
+}
+
+//================================RunTime================================
+
+// Running 是否在运行
+func (this *IReaderCloser) Running() bool {
+	return this.running == 1
+}
+
+func (this *IReaderCloser) Run() error {
+	if atomic.SwapUint32(&this.running, 1) == 1 {
+		return nil
+	}
+	for {
+		select {
+		case <-this.Done():
+			return this.Err()
+		default:
+			_ = this.CloseWithErr(func() (err error) {
+				defer func() {
+					if e := recover(); e != nil {
+						err = fmt.Errorf("%v", e)
+					}
+				}()
+				bytes, err := this.ReadMessage()
+				if err != nil || len(bytes) == 0 {
+					return err
+				}
+				//打印日志
+				this.IPrinter.Print(bytes, TagRead, this.GetKey())
+				//处理数据
+				if this.dealFunc != nil {
+					this.dealFunc(bytes)
+				}
+				return
+			}())
+		}
+	}
+}
---
+++ io_m_reader.go
@@ -0,0 +1,157 @@
+package io
+
+import (
+	"bufio"
+	"github.com/injoyai/io/buf"
+	"io"
+	"time"
+)
+
+func NewIReader(r Reader) *IReader {
+	return &IReader{
+		buf:      bufio.NewReader(r),
+		readFunc: buf.ReadWithAll,
+		lastChan: make(chan Message),
+	}
+}
+
+type IReader struct {
+	buf      *bufio.Reader                       //buffer
+	readFunc func(*bufio.Reader) ([]byte, error) //读取函数
+	lastChan chan Message                        //读取最新数据chan
+	lastTime time.Time                           //最后读取数据时间
+}
+
+//================================Nature================================
+
+// LastTime 最后数据时间
+func (this *IReader) LastTime() time.Time {
+	return this.lastTime
+}
+
+//================================Read================================
+
+// Buffer 极大的增加读取速度
+func (this *IReader) Buffer() *bufio.Reader {
+	return this.buf
+}
+
+// Read io.reader
+func (this *IReader) Read(p []byte) (int, error) {
+	return this.Buffer().Read(p)
+}
+
+// ReadByte 读取一字节
+func (this *IReader) ReadByte() (byte, error) {
+	return this.Buffer().ReadByte()
+}
+
+// ReadAll 读取全部数据
+func (this *IReader) ReadAll() ([]byte, error) {
+	return buf.ReadWithAll(this.Buffer())
+}
+
+// ReadMessage 实现MessageReader接口
+func (this *IReader) ReadMessage() ([]byte, error) {
+	if this.readFunc == nil {
+		return nil, ErrInvalidReadFunc
+	}
+	return this.readFunc(this.Buffer())
+}
+
+// ReadLast 读取最新的数据
+func (this *IReader) ReadLast(timeout time.Duration) (response []byte, err error) {
+	if timeout <= 0 {
+		select {
+		//case <-this.Ctx().Done():
+		//	err = this.Err()
+		case response = <-this.lastChan:
+		}
+	} else {
+		t := time.NewTimer(timeout)
+		select {
+		//case <-this.ctx.Done():
+		//	err = this.closeErr
+		case response = <-this.lastChan:
+		case <-t.C:
+			err = ErrWithTimeout
+		}
+	}
+	return
+}
+
+// WriteTo 写入io.Writer
+func (this *IReader) WriteTo(writer Writer) (int64, error) {
+	return Copy(writer, this)
+}
+
+// CopyTo 写入io.Writer
+func (this *IReader) CopyTo(writer Writer) (int64, error) {
+	return Copy(writer, this)
+}
+
+//================================ReadFunc================================
+
+// SetReadFunc 设置读取函数
+func (this *IReader) SetReadFunc(fn buf.ReadFunc) {
+	this.readFunc = func(reader *bufio.Reader) ([]byte, error) {
+		bs, err := fn(reader)
+		if err != nil {
+			return nil, err
+		}
+		if len(bs) > 0 {
+			//设置最后读取有效数据时间
+			this.lastTime = time.Now()
+			//尝试加入通道
+			select {
+			case this.lastChan <- bs:
+			default:
+			}
+		}
+		return bs, nil
+	}
+}
+
+// SetReadWithNil 设置读取函数为nil
+func (this *IReader) SetReadWithNil() {
+	this.SetReadFunc(nil)
+}
+
+// SetReadWithAll 一次性全部读取
+func (this *IReader) SetReadWithAll() {
+	this.SetReadFunc(buf.ReadWithAll)
+}
+
+// SetReadWithKB 读取固定字节长度
+func (this *IReader) SetReadWithKB(n uint) {
+	this.SetReadFunc(func(buf *bufio.Reader) ([]byte, error) {
+		bytes := make([]byte, n<<10)
+		length, err := buf.Read(bytes)
+		return bytes[:length], err
+	})
+}
+
+// SetReadWithStartEnd 设置根据包头包尾读取数据
+func (this *IReader) SetReadWithStartEnd(packageStart, packageEnd []byte) {
+	this.SetReadFunc(buf.NewReadWithStartEnd(packageStart, packageEnd))
+}
+
+// SetReadWithWriter same io.Copy 注意不能设置读取超时
+func (this *IReader) SetReadWithWriter(writer io.Writer) {
+	this.SetReadFunc(buf.NewReadWithWriter(writer))
+}
+
+// SetReadWithLenFrame 根据动态长度读取数据
+func (this *IReader) SetReadWithLenFrame(f *buf.LenFrame) {
+	this.SetReadFunc(buf.NewReadWithLen(f))
+}
+
+// SetReadWithTimeout 根据超时时间读取数据(需要及时读取,避免阻塞产生粘包)
+func (this *IReader) SetReadWithTimeout(timeout time.Duration) {
+	this.SetReadFunc(buf.NewReadWithTimeout(timeout))
+}
+
+// SetReadWithFrame 适配预大部分读取
+func (this *IReader) SetReadWithFrame(f *buf.Frame) {
+	this.SetReadFunc(buf.NewReadWithFrame(f))
+}
