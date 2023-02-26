2acb9ffef6766fbecb264193bc9468da9de3c227
钱纯净 <1113655791@qq.com>
2023-02-26 22:14:15 +0800

    增加测试最大连接数数据

--- dial/dial_dial.go
+++ dial/dial_dial.go
@@ -254,18 +254,4 @@ func (this *_websocket) Close() error {
 	return this.Conn.Close()
 }
 
-////================================ProxyDial================================
-//
-//func Proxy() (io.ReadWriteCloser, error) {
-//	return proxy.New(), nil
-//}
-//
-//func NewProxy() (*io.Client, error) {
-//	return io.NewDial(Proxy)
-//}
-//
-//func RedialProxy(fn ...func(ctx context.Context, c *io.Client)) *io.Client {
-//	return io.Redial(Proxy, fn...)
-//}
-
 //================================OtherDial================================
