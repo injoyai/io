c216ed2fedd28f5bf4330735106e6fa0f3b26986
injoy <1113655791@qq.com>
2023-09-22 14:39:16 +0800

    101

--- listen/listen_func.go
+++ listen/listen_func.go
@@ -2,13 +2,7 @@ package listen
 
 import (
 	"github.com/injoyai/io"
-	"github.com/injoyai/logs"
 )
-
-func init() {
-	//设置只打印到终端
-	logs.SetWriter(logs.Stdout)
-}
 
 func RunServer(s *io.Server, err error, options ...io.OptionServer) error {
 	if err != nil {
